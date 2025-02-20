12d8aca2b3768d48e2387323528d534a589218df
Dea Lana Asri <dl.asri@stud.uis.no>
2025-02-20 21:26:42 +0100

    first commit of project 2

---
+++ Project_2/basic.py
@@ -0,0 +1,9 @@
+# Basic Problem for Project 2: Modelling Traffic Flow
+# Created by: Dea Lana Asri - 277575
+# dl.asri@stud.uis.no
+# Date: 20.02.2025
+
+# Import necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
